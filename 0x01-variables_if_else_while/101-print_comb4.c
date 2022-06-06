7b772a4576d72a89883001b87c19933d9eca7860
SemTheLegend <semmathe@gmail.com>
2022-06-06 13:17:17 -0700

    if else while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int i, j , k;
+	int i, j, k;
 
 	for (i = 48; i < 58; i++)
 	{
@@ -18,7 +18,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 				putchar(k);
-			if (i != 55 || j != 56 || k != 57)
+				if (i != 55 || j != 56 || k != 57)
 				{
 					putchar(',');
 					putchar(' ');
