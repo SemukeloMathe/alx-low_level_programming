6ca7fa1c27966a500be9fc0bae2dd164677cd35f
SemTheLegend <semmathe@gmail.com>
2022-06-08 06:55:45 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print all alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	
+	i = 0;
+
+	while (i < 10)
+	{
+		char r;
+
+		for (r = 'a'; r <= 'z'; r++)
+		{ 
+			_putchar(r);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
