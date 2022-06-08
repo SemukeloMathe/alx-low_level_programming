ffceb02a14b59259b71babd96c04a38cebfa2b19
SemTheLegend <semmathe@gmail.com>
2022-06-08 06:59:59 -0700

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	
-	i = 0;
 
+	i = 0;
 	while (i < 10)
 	{
 		char r;
 
 		for (r = 'a'; r <= 'z'; r++)
-		{ 
+		{
 			_putchar(r);
 		}
 		_putchar('\n');
