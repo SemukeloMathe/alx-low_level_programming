4e48dd1b5a224bd207cc19328f577dbc7766ebcd
SemTheLegend <semmathe@gmail.com>
2022-06-10 06:41:14 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,8 @@ void print_triangle(int size)
 	int i, j, k;
 
 	if (size > 0)
-		_putchar(10);
+		_putchar('\n');
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -21,6 +22,6 @@ void print_triangle(int size)
 			else
 				_putchar('#');
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 }
