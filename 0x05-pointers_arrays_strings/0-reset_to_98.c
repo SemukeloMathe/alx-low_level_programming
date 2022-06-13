eef4ce7a364d777b857aa29ca2a92fe300804efc
SemTheLegend <semmathe@gmail.com>
2022-06-13 11:45:12 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * void reset to 98 - main
+ * @n: integer value
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	
+	n = 98;
+	int *pN = &n
+	return (0);
+}
