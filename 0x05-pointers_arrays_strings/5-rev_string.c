14165f16a8b05c90d493588600ab6ea723a02b6a
SemTheLegend <semmathe@gmail.com>
2022-06-14 12:48:06 -0700

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,23 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
-  * rev_string - reverses a string..
-  * @s: char pointer.
-  *
-  */
+ * rev_string - a function that reverse a string
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
+ */
 void rev_string(char *s)
 {
-	int len, index;
-	char *arr;
+	int len = 0, i = 0;
+	char aux;
 
-	while (s[len] != 0)
-	{
+	while (s[len] != '\0')
 		len++;
-	}
-	arr = (char *) malloc(len * sizeof(char));
-	for (index = 0; index <= len; index++)
+
+	while (i < len--)
 	{
-		arr[index] = (char) *(s + index);
-	}
-	for (index = 0; index <= len; index++)
-	{
-		*(s + index) = arr[len - index - 1];
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
 	}
 }
