ef1f065edb9b6a6bc65ab772418bb511ee133ce8
SemTheLegend <semmathe@gmail.com>
2022-06-17 13:54:57 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *char *infinite_add - function that adds two numbers
+ *@n1 - pointer to n1
+ *@n2 - pointer to n2
+ *@r - pointer to r
+ *size_r - integer
+ *Return: 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	char n1, n2, r;
+	int size_r;
+
+}
