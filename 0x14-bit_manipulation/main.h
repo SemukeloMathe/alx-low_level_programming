077de37fe0586daac7bf67503f37261243fc5f02
SemTheLegend <semmathe@gmail.com>
2022-07-22 14:32:36 -0700

    binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,7 +8,7 @@
  * File: main.h
  *
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ *       written in the 0x14-bit_manipulation directory.
  */
 
 int _putchar(char c);
