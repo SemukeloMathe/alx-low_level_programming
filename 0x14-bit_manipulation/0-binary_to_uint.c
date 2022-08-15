e02d96cc8017937f37a3a35dec53ca4ea1a98f61
SemTheLegend <semmathe@gmail.com>
2022-08-15 12:01:57 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,48 +1,29 @@
 #include "main.h"
 
-unsigned int _strlen(const char *str);
-
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: a string of 0 and 1 chars
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
  *
- * Return: the converted number. Otherwise 0
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int length, i, num;
+	int i;
+	unsigned int num;
 
-	if (b == NULL)
+	num = 0;
+	if (!b)
 		return (0);
-
-	length = _strlen(b);
-
-	for (i = 0, num = 0; i < length; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		num += (b[i] - '0') * (1 << (length - i - 1));
 	}
-
-	return (num);
-}
-
-/**
- * _strlen - finds the lengthof a given string
- * @str: string
- *
- * Return: the length of str
- */
-unsigned int _strlen(const char *str)
-{
-	unsigned int length = 0;
-
-	while (*str)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		length++;
-		str++;
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
 	}
-
-	return (length);
+	return (num);
 }
